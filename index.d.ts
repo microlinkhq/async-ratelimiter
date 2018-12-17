70feb8560c647dad392f2ebd105fd6c7c462e91e
Ayan Yenbekbay <ayan.yenb@gmail.com>
2018-12-17 19:14:12 +0100

    Add TS typings

---
+++ index.d.ts
@@ -0,0 +1,32 @@
+declare module 'async-ratelimiter' {
+  import Redis from 'ioredis';
+
+  namespace RateLimiter {
+    interface ConstructorOptions {
+      db: Redis.Redis;
+      max?: number;
+      duration?: number;
+      namespace?: string;
+      id?: string;
+    }
+
+    interface GetOptions {
+      id?: string;
+      max?: number;
+      duration?: number;
+    }
+
+    interface Status {
+      total: number;
+      remaining: number;
+      reset: number;
+    }
+  }
+
+  class RateLimiter {
+    constructor(options: RateLimiter.ConstructorOptions);
+    get(options: RateLimiter.GetOptions): Promise<RateLimiter.Status>;
+  }
+
+  export = RateLimiter;
+}
