a217b8efc7dcf633a9557ed62e93ba41075007f8
Andrew <5755685+arnidan@users.noreply.github.com>
2022-08-18 12:33:44 +0200

    fix: update ioredis types usage for v5

--- index.d.ts
+++ index.d.ts
@@ -1,33 +1,33 @@
 declare module 'async-ratelimiter' {
-  import Redis from 'ioredis';
+  import { Redis } from 'ioredis'
 
   namespace RateLimiter {
     interface ConstructorOptions {
-      db: Redis.Redis;
-      max?: number;
-      duration?: number;
-      namespace?: string;
-      id?: string;
+      db: Redis
+      max?: number
+      duration?: number
+      namespace?: string
+      id?: string
     }
 
     interface GetOptions {
-      id?: string;
-      max?: number;
-      duration?: number;
-      decrease?: boolean;
+      id?: string
+      max?: number
+      duration?: number
+      decrease?: boolean
     }
 
     interface Status {
-      total: number;
-      remaining: number;
-      reset: number;
+      total: number
+      remaining: number
+      reset: number
     }
   }
 
   class RateLimiter {
-    constructor(options: RateLimiter.ConstructorOptions);
-    get(options: RateLimiter.GetOptions): Promise<RateLimiter.Status>;
+    constructor(options: RateLimiter.ConstructorOptions)
+    get(options: RateLimiter.GetOptions): Promise<RateLimiter.Status>
   }
 
-  export = RateLimiter;
+  export = RateLimiter
 }
